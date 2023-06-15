54fa4e47ed1d14b85631ebfaef3f8796a6436bae
Lívia Nascimento <liviapnd@gmail.com>
2023-06-15 19:03:09 -0300

    Update styles.js

--- src/styles.js
+++ src/styles.js
@@ -1,7 +1,30 @@
 import styled from "styled-components";
 
 export const Container = styled.div `
-    width: 100%;
-    height: 100vw;
+    width: 100vw;
+    height: 100vh;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    background-color: #cbc3e3;
+     font-family: 'Poppins', sans-serif;
+`
+
+export const Content = styled.div`
+    border: 3px solid #303030;
+    border-radius: 0.5rem;
+    background-color: #e1d3d1;
+`
+
+export const ButtonsContent = styled.div`  
+    padding: 1.5rem 0.3rem;
+    border-radius: 1rem 1rem 0.37rem 0.37rem;
     background-color: #5b6499;
+`
+
+export const Row = styled.div`
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    align-items: center;
 `
